e9ce3350221a4131e4b5f61634a94d461cf0c700
“Charity-23” <charitymamah2@gmail.com>
2022-04-13 07:17:20 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,6 @@
 #include "main.h"
-                                                      /**
+
+/**
  * print_sign - Prints the sign of a number.           * @n: The number of which the sign will be printed.
  *                                                     * Return: 1 if the number is greater than zero,
  *         0 if the number is zero,
@@ -18,4 +19,4 @@ else
 {                                                     _putchar('-');
 return (-1);
 }
-}
+} 
