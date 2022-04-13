f1b2edd688e6fbe70f7dbc752be0e334c1e72a15
“Charity-23” <charitymamah2@gmail.com>
2022-04-13 10:27:46 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,8 +2,8 @@
 
 /**
  * jack bauer - prints every minute of the day of
- *              Jack Bauer, starting from 00:00
- *              to 23:59.
+ *              Jack Bauer, start from 00:00 to 23:59
+ *
  */
 void jack_bauer(void)
 {
