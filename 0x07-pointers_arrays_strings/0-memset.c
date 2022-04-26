b2e55f9f6339029aa1c62c3a4902b7fbe9ab40cc
“Charity-23” <charitymamah2@gmail.com>
2022-04-26 16:02:00 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * _memset - fills the first n bytes of the memory area pointed to by s with the constant byte b.
+ * _memset - fills the first n bytes of the memory ar
+ * a pointed to by s with the constant byte b.
  * @s: iput pointer to string
  * @b: constant char
  * @n: length of bytes
  * Return: pointer returning srting
  */
 
-char *_memsset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int t = 0;
 
