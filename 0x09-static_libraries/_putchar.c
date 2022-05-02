c5b65afe52b04e80dd96625adcdcad8dacf15978
“Charity-23” <charitymamah2@gmail.com>
2022-05-02 08:21:02 -0700

    task one

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - writes a single character to stdout.
+ * @c: the char to output.
+ * Return: 0
+ */
+
+int _putchar(char c)
+{
+	_putchar('c');
+		return (0);
+}
