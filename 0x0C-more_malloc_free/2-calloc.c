1701c5ea6f69a5748e97822ce500d23c3947fa04
“Charity-23” <charitymamah2@gmail.com>
2022-05-09 20:16:41 -0700

    task 3

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _memset - initializes n bytes of memory to x
+ * @ptr: initial address
+ * @x: variable to initialize with
+ * @n: number of bytes to initialize
+ *
+ * Return: return pointer char(so moved one byte)
+ */
+char *_memset(char *ptr, int x, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		ptr[i] = x;
+
+	return (ptr);
+}
+
+/**
+ * _calloc - allocates memory using malloc and
+ * initialize in 0
+ * @nmemb: number of elements of array to allocate
+ * @size: size of elements
+ * Return: pointer to allocated memory or normal
+ * process termination
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
+
+	if (nmemb == 0 || size == 0)
+		return (0);
+
+	p = malloc(nmemb * size);
+	if (p == 0)
+		return (0);
+	_memset(p, 0, size * nmemb);
+
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+A readme file elaborating malloc and free
