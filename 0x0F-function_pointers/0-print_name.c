16830b0fff95ce88292b00dce54c29659d15a769
“Charity-23” <charitymamah2@gmail.com>
2022-05-14 22:09:08 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,11 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name
- * @name: names's main
+ * print_name - print a name
+ * @name: name's main
  * @f: void function
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
