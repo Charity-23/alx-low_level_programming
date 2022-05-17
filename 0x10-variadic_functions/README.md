92eb5b1bea2959dccd745bb7ed87cab8ddf2d090
“Charity-23” <charitymamah2@gmail.com>
2022-05-17 16:39:19 -0700

    task 3

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+Introducing variadic functions in C
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,26 @@
+#ifndef variadic_functions_h
+#define variadic_functions_h
+
+/**
+ * struct fmt - Struct format
+ * @f: type format
+ * @p: print function
+ */
+typedef struct fmt
+{
+	char *f;
+	void (*p)(va_list s);
+} my_fmt;
+
+/*prototypes*/
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _strlen_recursion(char *s);
+void print_c(va_list s);
+void print_i(va_list s);
+void print_f(va_list s);
+void print_s(va_list s);
+
+#endif
